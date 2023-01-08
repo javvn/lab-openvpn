1fc257baf8bb5a4c0181a0e040928b3c02e85a98
javvn <juho0428@naver.com>
2023-01-08 20:56:49 +0900

    add sg openvpn

--- ec2/security-group.tf
+++ ec2/security-group.tf
@@ -1,9 +1,9 @@
-module "sg" {
+module "sg__ssh" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
 
-  name        = "${local.remote_state_context.name}-sg"
-  description = "Security Group for Open-vpn"
+  name        = "${local.remote_state_context.name}-ssh"
+  description = "Security Group for SSH"
   vpc_id      = local.vpc_id
 
   ingress_with_cidr_blocks = [
@@ -14,23 +14,55 @@ module "sg" {
       cidr_blocks = "0.0.0.0/0"
       description = "SSH"
     },
-    {
-      protocol    = "tcp"
-      from_port   = 80
-      to_port     = 80
-      cidr_blocks = "0.0.0.0/0"
-      description = "HTTP"
-    },
+    # {
+    #   protocol    = "tcp"
+    #   from_port   = 80
+    #   to_port     = 80
+    #   cidr_blocks = "0.0.0.0/0"
+    #   description = "HTTP"
+    # },
   ]
 
+  # egress_with_cidr_blocks = [
+  #   {
+  #     protocol    = "-1"
+  #     from_port   = 0
+  #     to_port     = 0
+  #     cidr_blocks = "0.0.0.0/0"
+  #   }
+  # ]
+
+  tags = merge(local.common_tags, { Name = "${local.remote_state_context.name}-ssh" })
+}
+
+module "sg__openvpn" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.16.2"
+
+  name        = "${local.remote_state_context.name}-openvpn"
+  description = "Security Group for OpenVPN"
+  vpc_id      = local.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      id          = "openvpn/all"
+      protocol    = "udp"
+      from_port   = 1194
+      to_port     = 1194
+      cidr_blocks = "0.0.0.0/0"
+      description = "Allow OpenVPN from anywhere."
+    }
+  ]
   egress_with_cidr_blocks = [
     {
+      id          = "all/all"
+      description = "Allow to communicate to the Internet."
       protocol    = "-1"
       from_port   = 0
       to_port     = 0
       cidr_blocks = "0.0.0.0/0"
-    }
+    },
   ]
 
-  tags = merge(local.common_tags, { Name = "${local.remote_state_context.name}-sg" })
+  tags = merge(local.common_tags, { Name = "${local.remote_state_context.name}-openvpn" })
 }
